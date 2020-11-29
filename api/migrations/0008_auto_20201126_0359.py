d1e5607335129a5401408e1d9ba00478a68bfcbe
jawish-a@outlook.com <jawish-a@outlook.com>
2020-11-29 11:57:34 +0300

    commneted json field for old verionw

--- api/migrations/0008_auto_20201126_0359.py
+++ api/migrations/0008_auto_20201126_0359.py
@@ -22,7 +22,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('reference', models.CharField(max_length=50)),
                 ('date', models.DateTimeField(auto_now=True)),
-                ('tap_response_json', models.JSONField()),
+                # ('tap_response_json', models.JSONField()),
                 ('order', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='payment', to='api.order')),
             ],
         ),
